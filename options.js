ab70b06a3bb2311298445d2110ff7ef254ebcc4b
Kathleen Yu <kathyu8@gmail.com>
2016-04-18 23:22:29 -0400

    added three settings

--- options.js
+++ options.js
@@ -1,33 +1,43 @@
-// Saves options to chrome.storage.sync.
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+  var num_of_opinions = $("#num_of_opinions").val();
+  var is_accelerated = $("#is_accelerated").prop("checked");
+  var is_customize_question = $("#is_question_description").prop("checked");
+
+  // Saves options to chrome.storage.sync.
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    num_of_opinions: num_of_opinions,
+    is_accelerated: is_accelerated,
+    is_customize_question: is_customize_question
   }, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    var status = $("#status");
+    status.text("Options saved.");
     setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+      status.text('');
+    }, 1000);
   });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
+  // Use default value num_of_opinions = '10' and is_accelerated = false.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    num_of_opinions: '10',
+    is_accelerated: false,
+    is_customize_question: false
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    $("#num_of_opinions").val(items.num_of_opinions);
+    $("#is_accelerated").prop("checked", items.is_accelerated);
+    $("#is_question_description").prop("checked", items.is_customize_question);
   });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+$(document).ready(function(){
+  restore_options();
+  
+  $("#save").click(function(e){
+    save_options();
+  });
+
+});
