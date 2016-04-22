36860f399042f4fa3925c85515e31fe765f8c0c0
Kathleen Yu <kathyu8@gmail.com>
2016-04-21 20:58:33 -0400

    Added option to adjust amount paid per worker; Need to add regression equation later

--- options.js
+++ options.js
@@ -1,5 +1,7 @@
 function save_options() {
   var num_of_opinions = $("#num_of_opinions").val();
+  var payment_rate = $("#payment_rate").val();
+  var time_estimate = $("#time_estimate").text();
   var is_accelerated = $("#is_accelerated").prop("checked");
   var is_customized = $("#is_customized").prop("checked");
   var question, description = "";
@@ -12,6 +14,8 @@ function save_options() {
   // Saves options to chrome.storage.sync.
   chrome.storage.sync.set({
     num_of_opinions: num_of_opinions,
+    payment_rate: payment_rate,
+    time_estimate: time_estimate,
     is_accelerated: is_accelerated,
     is_customized: is_customized,
     question: question,
@@ -30,13 +34,17 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   chrome.storage.sync.get({
-    num_of_opinions: '10',
+    num_of_opinions: 10,
+    payment_rate: 5,
+    time_estimate: 50,
     is_accelerated: false,
     is_customized: false,
     question: "",
     description: ""
   }, function(items) {
     $("#num_of_opinions").val(items.num_of_opinions);
+    $("#payment_rate").val(items.payment_rate);
+    $("#time_estimate").text(items.time_estimate);
     $("#is_accelerated").prop("checked", items.is_accelerated);
     $("#is_customized").prop("checked", items.is_customized);
     $("#question").val(items.question);
@@ -52,8 +60,20 @@ function restore_options() {
   });
 }
 
+function time_estimate(num_of_opinions, payment_rate){
+  var a = 60/payment_rate;
+  var b = num_of_opinions;
+  return a;
+}
+
 $(document).ready(function(){
   restore_options();
+
+  $("#num_of_opinions, #payment_rate").change(function(){
+    var num_of_opinions = $("#num_of_opinions").val();
+    var payment_rate = $("#payment_rate").val(); 
+    $("#time_estimate").text(time_estimate(num_of_opinions, payment_rate));
+  });
 
   $("#is_customized").change(function() {
     if($(this).prop("checked")) {
